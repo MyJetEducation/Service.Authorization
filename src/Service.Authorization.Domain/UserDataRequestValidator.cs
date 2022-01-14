5c10e894db157affb9a2b198fee4acb564d01659
dmitriy.b <dmitriy.b@smplt.net>
2022-01-14 10:27:21 +0500

    Add validator

---
+++ src/Service.Authorization.Domain/UserDataRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Service.Authorization.Domain
+{
+	public class UserDataRequestValidator
+	{
+		private static readonly Regex PasswordRegex = new Regex("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,31}$", RegexOptions.Compiled);
+		private static readonly Regex FullNameRegex = new Regex("^([A-Za-z]+)\\ ([A-Za-z]+)$", RegexOptions.Compiled);
+
+		public static bool ValidatePassword(string value) => PasswordRegex.IsMatch(value);
+
+		public static bool ValidateLogin(string value) => new EmailAddressAttribute().IsValid(value);
+
+		public static bool ValidateFullName(string value) => FullNameRegex.IsMatch(value);
+	}
+}
