a2affc94cd006067cb0c1c0f44e4fc72cb7dafb0
dmitriy.b <dmitriy.b@smplt.net>
2022-02-17 09:19:01 +0500

    FIN360PMVP-148: Эндпоинт /register/create кушает не валидный email#resolved

--- src/Service.Authorization.Client/Services/UserDataRequestValidator.cs
+++ src/Service.Authorization.Client/Services/UserDataRequestValidator.cs
@@ -1,17 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
+using Service.Core.Client.Extensions;
 
 namespace Service.Authorization.Client.Services
 {
 	public class UserDataRequestValidator
 	{
-		private static readonly Regex PasswordRegex = new Regex("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,31}$", RegexOptions.Compiled);
-		private static readonly Regex FullNameRegex = new Regex("^([A-Za-z]+)\\ ([A-Za-z]+)$", RegexOptions.Compiled);
+		private static readonly Regex PasswordRegex = new("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,31}$",
+			RegexOptions.Compiled);
 
-		public static bool ValidatePassword(string value) => PasswordRegex.IsMatch(value);
+		private static readonly Regex FullNameRegex = new("^([A-Za-z]+)\\ ([A-Za-z]+)$",
+			RegexOptions.Compiled);
 
-		public static bool ValidateLogin(string value) => new EmailAddressAttribute().IsValid(value);
+		private static readonly Regex EmailRegex = new(@"^([0-9a-zA-Z]([\+\-_\.][0-9a-zA-Z]+)*)+@(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]*\.)+[a-zA-Z0-9]{2,17})$",
+			RegexOptions.Compiled | RegexOptions.ExplicitCapture | RegexOptions.IgnoreCase);
 
-		public static bool ValidateFullName(string value) => FullNameRegex.IsMatch(value);
+		public static bool ValidatePassword(string value) => !value.IsNullOrWhiteSpace() && PasswordRegex.IsMatch(value);
+
+		public static bool ValidateLogin(string value) => !value.IsNullOrWhiteSpace() && EmailRegex.IsMatch(value);
+
+		public static bool ValidateFullName(string value) => !value.IsNullOrWhiteSpace() && FullNameRegex.IsMatch(value);
 	}
 }
---
+++ test/Service.Authorization.Client.Tests/UserDataRequestValidatorTests.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using Service.Authorization.Client.Services;
+
+namespace Service.Authorization.Client.Tests
+{
+	public class UserDataRequestValidatorTests
+	{
+		[TestCase("abc@mail.com")]
+		[TestCase("abc@ma-il.com")]
+		[TestCase("abc1@mail2.cc")]
+		[TestCase("abc-d@mail.com")]
+		[TestCase("abc.def@mail.com")]
+		[TestCase("abc_def@mail.com")]
+		[TestCase("l@d.ru")]
+		[TestCase("ma@hostname.com")]
+		[TestCase("ma@hostname.comcom")]
+		[TestCase("MA@hostname.coMCom")]
+		[TestCase("m.a@hostname.co")]
+		[TestCase("m_a1a@hostname.com")]
+		[TestCase("ma-a@hostname.com")]
+		[TestCase("ma-a@hostname.com.edu")]
+		[TestCase("ma-a.aa@hostname.com.edu")]
+		[TestCase("ma.h.saraf.onemore@hostname.com.edu")]
+		[TestCase("ma12@hostname.com")]
+		[TestCase("12@hostname.com")]
+		public void ValidateLoginReturn_true_if_email_is_valid(string mail)
+		{
+			bool isValid = UserDataRequestValidator.ValidateLogin(mail);
+
+			Assert.IsTrue(isValid);
+		}
+
+		[TestCase(null)]
+		[TestCase("")]
+		[TestCase("abc")]
+		[TestCase("abc@")]
+		[TestCase("@mail.com")]
+		[TestCase("[a]bc@mail.com")]
+		[TestCase("a[b]c@mail.com")]
+		[TestCase("abc@m[a]il.com")]
+		[TestCase("abc@mail")]
+		[TestCase("abc-@mail.com")]
+		[TestCase("abc..def@mail.com")]
+		[TestCase(".abc@mail.com")]
+		[TestCase("abc#def@mail.com")]
+		[TestCase("abc.def@mail.c")]
+		[TestCase("abc.def@mail#archive.com")]
+		[TestCase("abc.def@mail")]
+		[TestCase("abc.def@mail..com")]
+		[TestCase("l@d.c")]
+		[TestCase("Abc.example.com")]
+		[TestCase("A@b@c@example.com")]
+		[TestCase("ma...ma@jjf.co")]
+		[TestCase("ma@@jjf.com")]
+		[TestCase("ma.@jjf.com")]
+		[TestCase("ma_@jjf.com")]
+		[TestCase("ma_@jjf")]
+		[TestCase("ma_@jjf.")]
+		[TestCase("ma@jjf.")]
+		public void ValidateLoginReturn_false_if_email_is_not_valid(string mail)
+		{
+			bool isValid = UserDataRequestValidator.ValidateLogin(mail);
+
+			Assert.IsFalse(isValid);
+		}
+	}
+}
