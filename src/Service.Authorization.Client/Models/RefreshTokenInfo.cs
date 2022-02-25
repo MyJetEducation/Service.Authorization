c8486ae5278fcd0bffc2df69c6c85080e020c430
dmitriy.b <dmitriy.b@smplt.net>
2022-02-25 12:16:08 +0500

    Refactoring: Contract fixed

--- src/Service.Authorization.Client/Models/RefreshTokenInfo.cs
+++ src/Service.Authorization.Client/Models/RefreshTokenInfo.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace Service.Authorization.Client.Models
 {
+	[DataContract]
 	public class RefreshTokenInfo
 	{
+		[DataMember(Order = 1)]
 		public Guid? RefreshTokenUserId { get; set; }
 
+		[DataMember(Order = 2)]
 		public DateTime RefreshTokenExpires { get; set; }
-		
+
+		[DataMember(Order = 3)]
 		public string RefreshTokenIpAddress { get; set; }
 	}
 }
